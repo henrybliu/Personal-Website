e6a99567fcf257d5bc5fdd61fa6e763ee372f818
Henry Liu <11.henrybliu@gmail.com>
2024-01-02 17:40:15 -0800

    start on portfolio section

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,49 +13,52 @@ const Footer = () => {
   return (
     <div className="footerContainer">
       <div className="footerContent">
-        <div className="leftFooter">
-          <div className="topLeftFooter">
-            <h1>Thanks for visiting!</h1>
+        <div className="topFooter">
+          <div className="leftFooter">
+            <div className="topLeftFooter">
+              <h1>Thanks for visiting!</h1>
+            </div>
+            <h2>
+              I'd love to hear from you! Please feel free to reach out with any
+              questions, comments, or just to say hello.
+            </h2>
           </div>
-          <h2>
-            I'd love to hear from you! Please feel free to reach out with any
-            questions, comments, or just to say hello.
-          </h2>
+          <div className="rightFooter">
+            <a
+              href="https://www.linkedin.com/in/henrybliu/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkedIn"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a
+              href="https://github.com/henrybliu"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="github"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a
+              href="mailto:11.henrybliu@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="email"
+            >
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+            <a
+              href="https://www.instagram.com/xhenryliu/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="github"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
         </div>
-        <div className="rightFooter">
-          <a
-            href="https://www.linkedin.com/in/henrybliu/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="linkedIn"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-          <a
-            href="https://github.com/henrybliu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="github"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-          <a
-            href="mailto:11.henrybliu@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="email"
-          >
-            <FontAwesomeIcon icon={faEnvelope} />
-          </a>
-          <a
-            href="https://www.instagram.com/xhenryliu/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="github"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-        </div>
+        <h3> Henry Liu © 2024</h3>
       </div>
     </div>
   );
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "../style/portfolio.css";
 
+import Dash from "../components/dash";
+
 const Portfolio = () => {
   return (
     <div className="portfolioContainer" id="portfolio">
-      Portfolio section
+      <div className="portfolioContent">
+        <div className="topPortfolio">
+          <div className="portfolioTitle">
+            <Dash></Dash>
+            <h1>My portfolio 🎨</h1>
+          </div>
+          <div className="filters"> Filters</div>
+        </div>
+        <div className="showcase">showcase section</div>
+      </div>
     </div>
   );
 };
