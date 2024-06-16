8e49de192fdb09387bf1c89b0047dcf5d9a63cfd
Henry Liu <11.henrybliu@gmail.com>
2024-06-15 21:58:23 -0700

    update about me

--- src/sections/about.js
+++ src/sections/about.js
@@ -23,13 +23,13 @@ const About = () => {
             </div>
 
             <h2>
-              I'm a Computer Engineering student at the University of
-              California, San Diego with experience in full stack development,
-              network software engineering, and machine learning.
+              I'm a Master's student at the University of California, San Diego
+              studying Machine Learning & Data Science with experience in full
+              stack development, software engineering, and machine learning.
             </h2>
             <h2 id="bottomH2">
               In my free time, I love snowboarding, soccer, and trying new
-              recipes. Currently, I am training for my first half-marathon!
+              recipes. Currently, I am training for my first Spartan race!
             </h2>
             <div className="buttons">
               <Btn2
